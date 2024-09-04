7b4c49c5fdfc6339a6579ac68b369bd8c296223d
Bilooty <bill.j.rule@gmail.com>
2024-09-04 15:56:05 +1000

    Moved PlantTypes.json to the data folder

--- App/other/ExploreDisplayPlants.tsx
+++ App/other/ExploreDisplayPlants.tsx
@@ -3,7 +3,7 @@ import React, {useEffect} from 'react'
 import {FlatList, Text, View} from "react-native";
 import * as fs from 'react-native-fs'
 import {readString} from 'react-native-csv'
-import plants from "../PlantTypes.json";
+import plants from "../data/PlantTypes.json";
 
 
 export const ExploreDisplayPlants = ({}) => {
