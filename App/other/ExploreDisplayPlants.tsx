a587761f31255c34700aff1f6c16d02546f0aa20
Bilooty <bill.j.rule@gmail.com>
2024-09-04 14:20:23 +1000

    Load plants

--- App/other/ExploreDisplayPlants.tsx
+++ App/other/ExploreDisplayPlants.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import {View, Text} from 'react-native';
 
@@ -12,3 +13,57 @@ export const ExploreDisplayPlants = () => {
         </View>
     );
 };
+=======
+
+import React, {useEffect} from 'react'
+import {View} from "react-native";
+import fs from "fs";
+import {parse} from 'csv-parse'
+const loadCSV =  function (filePath: string) {
+    return new Promise((resolve, reject) => {
+        const results: any[] = [];
+
+        fs.createReadStream(filePath)
+            .pipe(parse({columns: true}))
+            .on('data', (row) => results.push(row))
+            .on('end', () => {
+
+                resolve(results); // Resolve the promise with the current instance for chaining
+            })
+            .on('error', (err) => reject(err)); // Reject the promise in case of an error
+
+    });
+}
+
+type PlantType = {
+    name:string,
+    sci_name:string,
+    difficulty:string,
+    moisture_min:number,
+    moisture_max:number,
+    sun_min:number,
+    sun_max:number,
+    temp_min:number,
+    temp_max:number,
+    uv_min:number,
+    uv_max:number,
+
+
+}
+export const ExploreDisplayPlants = ({}) => {
+    const [isLoading, setIsLoading] = React.useState(true);
+    const [data, setData] = React.useState([]);
+
+    const getPlants = async () => {
+        setData(await loadCSV("./data/PlantTypes") as never)
+    }
+    useEffect(() => {
+        getPlants().then()
+    }, []);
+    return (
+        <View>
+            data
+        </View>
+    )
+}
+>>>>>>> Stashed changes
