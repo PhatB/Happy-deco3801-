93367985ff353b7efc25f9779a8632456948c3c9
Bilooty <bill.j.rule@gmail.com>
2024-09-13 10:44:25 +1000

    Attempted getting data from server

--- App/pages/HomeScreen.tsx
+++ App/pages/HomeScreen.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView, View, Text, Image} from 'react-native';
 
 import {styles} from '../other/Styles.tsx';
 import {Footer} from '../other/Footer.tsx';
 import {Search} from '../other/Search.tsx';
 
-
+const testUserId = "66e3848cc14bcef4f162d6e9";
 const plusIcon = '../images/plusIcon.png';
 import {PrettyList} from '../other/PrettyList.tsx';
 import plants from "../data/PlantTypes.json";
 type UserPlant = {
     id: string;
     name: string,
-
+    plantType: string,
+    device: string,
 }
 export const HomeScreen = () => {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState<UserPlant[]>([]);
+    const [error, setError] = useState<String | null>(null);
+    const getPlants = async () => {
+        try {
+            const response = await fetch("https://deco3801-teamhappy.uqcloud.net/api/UserPlants/FromUser/" + testUserId);
+            const json = await response.json();
+            setData(json);
+        } catch (err) {
+            setError(err as String);
+        } finally {
+            setLoading(false);
+        }
+    }
     return (
     // Full view
         <View style={{height: '100%'}}>
@@ -36,7 +51,10 @@ export const HomeScreen = () => {
                             <Image source={require(plusIcon)} />
                             <Text style={styles.greenButton}>{'Add new plant'}</Text>
                         </View>
-                        <PrettyList data={plants} />
+                        error === null
+                        ? <Text>Error while accessing your plant list: {error}</Text>
+                        : <PrettyList data={data} />
+
                     </View>
                 </ScrollView>
             </View>
