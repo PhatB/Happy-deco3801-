9e5cbcadd9ec683a46ed82c4a736535ce6a7d244
Caletin <cmikli03@gmail.com>
2024-09-28 18:51:12 +1000

    Adds buttons to profile screen

--- App/pages/ProfileScreen.tsx
+++ App/pages/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView, View, Text} from 'react-native';
+import {ScrollView, View, Text, Pressable, Image} from 'react-native';
 
 import {styles} from '../other/Styles.tsx';
 import {Footer} from '../other/Footer.tsx';
@@ -12,6 +12,51 @@ export const ProfileScreen = () => {
             <View style={styles.scrollArea}>
                 <ScrollView contentInsetAdjustmentBehavior="automatic">
                     <Text style={styles.pageTitle}>{'Profile'}</Text>
+
+                    {/* Account */}
+                    <View style={[styles.main, {flexDirection: 'row', paddingVertical: 20, marginTop: '22.5%'}]}>
+                        <View style={{width: '92.5%'}}>
+                            <Text style={styles.smallHeading}>Account</Text>
+                        </View>
+                        <Pressable>
+                            <Image
+                                style={{direction: 'rtl', width: 20, height: 20}}
+                                source={require('../images/arrowIcon.png')}
+                            />
+                        </Pressable>
+                    </View>
+
+                    {/* Notification */}
+                    <View style={[styles.main, {flexDirection: 'row', paddingVertical: 20}]}>
+                        <View style={{width: '92.5%'}}>
+                            <Text style={styles.smallHeading}>Notification</Text>
+                        </View>
+                        <Pressable>
+                            <Image
+                                style={{direction: 'rtl', width: 20, height: 20}}
+                                source={require('../images/arrowIcon.png')}
+                            />
+                        </Pressable>
+                    </View>
+
+                    {/* Help & Privacy */}
+                    <View style={[styles.main, {flexDirection: 'row', paddingVertical: 20}]}>
+                        <View style={{width: '92.5%'}}>
+                            <Text style={styles.smallHeading}>Help & Privacy</Text>
+                        </View>
+                        <Pressable>
+                            <Image
+                                style={{direction: 'rtl', width: 20, height: 20}}
+                                source={require('../images/arrowIcon.png')}
+                            />
+                        </Pressable>
+                    </View>
+
+                    {/* Logout */}
+                    <View style={[styles.main, {flexDirection: 'row', paddingVertical: 20, backgroundColor: '#F5D4C9', justifyContent: 'center'}]}>
+                            <Text style={styles.smallHeading}>Logout</Text>
+                    </View>
+
                 </ScrollView>
             </View>
 
