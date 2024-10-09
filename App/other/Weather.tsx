8900066d738f288997644b46f3c27f5ece064fa2
Caletin <cmikli03@gmail.com>
2024-10-09 18:25:22 +1000

    Moves weather fetching into separate function with useCallback and async

--- App/other/Weather.tsx
+++ App/other/Weather.tsx
@@ -1,28 +1,60 @@
-import React from 'react';	
+import React, {useCallback} from 'react';	
 import { fetchWeatherApi } from 'openmeteo';
-import {ScrollView, View, Text, Image, Pressable} from 'react-native';
+import {Alert, ScrollView, View, Text, Image, Pressable} from 'react-native';
 
+export const Weather = () => {
+    const params = {
+        "latitude": 52.52,
+        "longitude": 13.41,
+        "hourly": "temperature_2m"
+    };
+    const url = "https://api.open-meteo.com/v1/forecast";
 
+    // Helper function to form time ranges
+    const range = (start: number, stop: number, step: number) =>
+        Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
 
+    {/* Get weather */}
+    const getWeather = useCallback(async () => {
+        try {
+            {/* Responses */}
+            const responses = await fetchWeatherApi(url, params);
+            const response = responses[0];
 
+            const utcOffsetSeconds = response.utcOffsetSeconds();
+            const timezone = response.timezone();
+            const timezoneAbbreviation = response.timezoneAbbreviation();
+            const latitude = response.latitude();
+            const longitude = response.longitude();
+            const hourly = response.hourly()!;
 
-export const Weather = async () => {
-    // const params = {
-    //     "latitude": 52.52,
-    //     "longitude": 13.41,
-    //     "hourly": "temperature_2m"
-    // };
-    // const url = "https://api.open-meteo.com/v1/forecast";
-    // const responses = await fetchWeatherApi(url, params);
+            const weatherData = {
+                hourly: {
+                    time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
+                        (t) => new Date((t + utcOffsetSeconds) * 1000)
+                    ),
+                    temperature2m: hourly.variables(0)!.valuesArray()!,
+                },
+            };
+            for (let i = 0; i < weatherData.hourly.time.length; i++) {
+                console.log(
+                    weatherData.hourly.time[i].toISOString(),
+                    weatherData.hourly.temperature2m[i]
+                );
+            };
+
+            //return (timezone);
+        }
+        catch(e) {
+            {/* Error */}
+            Alert.alert(`Couldn't fetch weather API.`);
+        }
+    }, [url]);
     
-    // // Helper function to form time ranges
-    // const range = (start: number, stop: number, step: number) =>
-    //     Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+    // Process first location. Add a for-loop for multiple locations or weather models
+    //const response = responses[0];
     
-    // // Process first location. Add a for-loop for multiple locations or weather models
-    // const response = responses[0];
-    
-    // // Attributes for timezone and location
+    // Attributes for timezone and location
     // const utcOffsetSeconds = response.utcOffsetSeconds();
     // const timezone = response.timezone();
     // const timezoneAbbreviation = response.timezoneAbbreviation();
@@ -31,7 +63,7 @@ export const Weather = async () => {
     
     // const hourly = response.hourly()!;
     
-    // // Note: The order of weather variables in the URL query and the indices below need to match!
+    // Note: The order of weather variables in the URL query and the indices below need to match!
     // const weatherData = {
     
     //     hourly: {
@@ -43,7 +75,7 @@ export const Weather = async () => {
     
     // };
     
-    // // `weatherData` now contains a simple structure with arrays for datetime and weather data
+    // `weatherData` now contains a simple structure with arrays for datetime and weather data
     // for (let i = 0; i < weatherData.hourly.time.length; i++) {
     //     console.log(
     //         weatherData.hourly.time[i].toISOString(),
@@ -52,9 +84,8 @@ export const Weather = async () => {
     // };
     return(
         <View>
-            <Text>
-                aaaaaa
-            </Text>
+            <Text>aaaaaa</Text>
+            <Text>{`${getWeather()}`}</Text>
         </View>
             
     );
