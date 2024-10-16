88adb10ba5a5c3329ce2cad9c926ada6f9808e98
Bilooty <bill.j.rule@gmail.com>
2024-10-16 13:15:26 +1000

    Can now add description to plant

--- App/pages/AddPlantScreen.tsx
+++ App/pages/AddPlantScreen.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import {ScrollView, View, Text, Image, Pressable, TextInput, Modal} from 'react-native';
+import { ScrollView, View, Text, Image, Pressable, TextInput, Modal } from 'react-native';
 
-import {styles} from '../other/Styles.tsx';
-import {Footer} from '../other/Footer.tsx';
+import { styles } from '../other/Styles.tsx';
+import { Footer } from '../other/Footer.tsx';
 
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 
-import {BackButton} from "../other/MiscComponents/BackButton.tsx";
+import { BackButton } from "../other/MiscComponents/BackButton.tsx";
 import { Dropdown } from 'react-native-element-dropdown';
 import { addUserPlant, getDevice, getPlantTypes, UserPlant } from '../api.ts';
 import { Loading } from '../other/MiscComponents/Loading.tsx';
@@ -17,6 +17,7 @@ export const AddPlantScreen = () => {
     const [plantName, setPlantName] = useState("");
     const [plantTypes, setPlantTypes] = useState([]);
     const [deviceID, setDeviceID] = useState("");
+    const [description, setDescription] = useState("");
     const [selectedPlantType, setSelectedPlantType] = useState("");
     const [issues, setIssues] = useState<string>("");
     const [infoVisible, setInfoVisible] = useState(false);
@@ -29,7 +30,7 @@ export const AddPlantScreen = () => {
             name: plantName,
             device: deviceID,
             plantType: selectedPlantType,
-            description: ""
+            description: description
         }
         console.log(plant)
         try {
@@ -37,12 +38,12 @@ export const AddPlantScreen = () => {
         } catch (e: any) {
             console.error("ERROR: " + e.message)
         }
-        navigation.navigate("Home",  {screen: "HomeScreen"})
+        navigation.navigate("Home", { screen: "HomeScreen" })
     }
     const setupPlantTypes = async () => {
         try {
-        setPlantTypes(await getPlantTypes())
-        }catch(e: any) {
+            setPlantTypes(await getPlantTypes())
+        } catch (e: any) {
             console.error(e.message);
         }
     }
@@ -62,7 +63,7 @@ export const AddPlantScreen = () => {
     const checkValidity = async () => {
         let valid = true;
         let newIssues = ""
-        
+
         let exists = await deviceExists()
         if (!exists) {
             valid = false;
@@ -72,60 +73,60 @@ export const AddPlantScreen = () => {
             valid = false;
             newIssues += "\nPlease select a plant type."
         }
-        setIssues(newIssues)       
+        setIssues(newIssues)
         return valid;
     }
 
     const InfoModal = () => {
         return (
-        <Modal
-            animationType='fade'
-            visible={infoVisible}
-            transparent={true}
-            onRequestClose={() => {setInfoVisible(false)}}
-        >
-            <View style={[styles.modalView, styles.main]}>
-                <Text style={styles.smallHeading}>Finding your device ID</Text>
-                <Image style={{borderRadius:100, marginVertical:20}}source={require("../images/device_diagram.png")}></Image>
-                <Text style={{color:"black", fontSize: 18}}>
-                    The serial number of your HappyPlants device is located on a small
-                    sticker near the bottom of the device.
-                </Text>
-                <Pressable onPress={() => {
-                    setInfoVisible(false)
-                }}
-                style={[styles.smallGreenButton, {marginVertical: 15}]}>
-                            <Text style={[styles.greenButton, {textAlign: "center"}]}>{'Got it.'}</Text>
-                </Pressable>
-            </View>
-           
-        </Modal>
+            <Modal
+                animationType='fade'
+                visible={infoVisible}
+                transparent={true}
+                onRequestClose={() => { setInfoVisible(false) }}
+            >
+                <View style={[styles.modalView, styles.main]}>
+                    <Text style={styles.smallHeading}>Finding your device ID</Text>
+                    <Image style={{ borderRadius: 100, marginVertical: 20 }} source={require("../images/device_diagram.png")}></Image>
+                    <Text style={{ color: "black", fontSize: 18 }}>
+                        The serial number of your HappyPlants device is located on a small
+                        sticker near the bottom of the device.
+                    </Text>
+                    <Pressable onPress={() => {
+                        setInfoVisible(false)
+                    }}
+                        style={[styles.smallGreenButton, { marginVertical: 15 }]}>
+                        <Text style={[styles.greenButton, { textAlign: "center" }]}>{'Got it.'}</Text>
+                    </Pressable>
+                </View>
+
+            </Modal>
         )
     }
 
     useEffect(() => {
         setupPlantTypes().then();
-    },[])
+    }, [])
     return (
         // Full view
-        <View style={{height: '100%'}}>
+        <View style={{ height: '100%' }}>
             {/* Scroll view */}
             <View style={styles.scrollArea}>
                 <ScrollView contentInsetAdjustmentBehavior="automatic">
                     <Text style={styles.pageTitle}>{'Add a Plant'}</Text>
-                    <BackButton/>
-                    <InfoModal/>
+                    <BackButton />
+                    <InfoModal />
 
 
                     <View style={styles.main}>
-                        <View style={{padding: 8, gap: 20}}>
+                        <View style={{ padding: 8, gap: 20 }}>
                             {/* Plant Name */}
                             <Text style={styles.smallHeading}>{'Plant Name'}</Text>
                             <TextInput
-                            style={{...styles.smallWhiteButton,...styles.textBox}}
-                            keyboardType="default"
-                            onChangeText={setPlantName}
-                            maxLength={20}
+                                style={{ ...styles.smallWhiteButton, ...styles.textBox }}
+                                keyboardType="default"
+                                onChangeText={setPlantName}
+                                maxLength={20}
                             />
                             {/* Plant Type */}
                             <Text style={styles.smallHeading}>{'Plant Type'}</Text>
@@ -133,7 +134,7 @@ export const AddPlantScreen = () => {
                                 data={plantTypes}
                                 labelField={"name"}
                                 valueField={"id"}
-                                style={{...styles.smallWhiteButton, ...styles.textBox}}
+                                style={{ ...styles.smallWhiteButton, ...styles.textBox }}
                                 fontFamily='Poppins-Regular'
                                 placeholder='Select a plant'
                                 onChange={(item: any) => {
@@ -143,34 +144,35 @@ export const AddPlantScreen = () => {
                             {/* Description */}
                             <Text style={styles.smallHeading}>{'Description (optional)'}</Text>
                             <TextInput
-                            style={{...styles.smallWhiteButton,...styles.textBox}}
-                            keyboardType="default"
+                                style={{ ...styles.smallWhiteButton, ...styles.textBox }}
+                                keyboardType="default"
+                                onChangeText={setDescription}
                             />
-                            <View style={[{display: "flex", flexDirection: "row", alignItems:"center"}]}>
-                                <Text style={[styles.smallHeading, {flex: 1}]}>{'Device ID'}</Text>
+                            <View style={[{ display: "flex", flexDirection: "row", alignItems: "center" }]}>
+                                <Text style={[styles.smallHeading, { flex: 1 }]}>{'Device ID'}</Text>
                                 <Pressable onPress={() => {
                                     setInfoVisible(true)
-                                }} style={{flex:1, alignItems:"flex-end"}}>
-                                    <Image style={{width:30, height: 30}}source={require("../images/help_icon.png")}/>
+                                }} style={{ flex: 1, alignItems: "flex-end" }}>
+                                    <Image style={{ width: 30, height: 30 }} source={require("../images/help_icon.png")} />
                                 </Pressable>
                             </View>
-                            
+
                             <TextInput
-                            style={{...styles.smallWhiteButton,...styles.textBox}}
-                            keyboardType="default"
-                            onChangeText={setDeviceID}
+                                style={{ ...styles.smallWhiteButton, ...styles.textBox }}
+                                keyboardType="default"
+                                onChangeText={setDeviceID}
                             />
                             <Image
                                 style={styles.line}
                                 source={require("../images/line.png")}
                             />
                             {/* Device */}
-                           
+
                         </View>
                         <Text style={styles.error}>{issues}</Text>
                         <Pressable
-                        onPress={submitPlant}
-                        style={[styles.greenButton, {marginVertical: 15}]}>
+                            onPress={submitPlant}
+                            style={[styles.greenButton, { marginVertical: 15 }]}>
                             <Text style={[styles.greenButton]}>{'Plant now!'}</Text>
                         </Pressable>
                     </View>
